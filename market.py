3a87f99a7339ed1e7e57e986ce448b1875d60d60
taahahussainkhan <taaha128@gmail.com>
2024-01-04 17:10:37 +0500

    added homepage and Navbar

--- market.py
+++ market.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello'
-
+    return render_template('home.html')
 
 
 
